372812bde1d55c0e6af02815f01b04c7cf2adc72
Marco-ICT! <marco.jilderts.jilderts@student.nhlstenden.com>
2023-10-09 13:32:34 +0200

    commit

---
+++ beatemapp/Leaderboard.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace BeatEmApp
+{
+    /// <summary>
+    /// Interaction logic for Leaderboard.xaml
+    /// </summary>
+    public partial class Leaderboard : Window
+    {
+        public Leaderboard()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public static class NavigationService
+    {
+        static NavigationService()
+        {
+            
+        }
+    }
+}
